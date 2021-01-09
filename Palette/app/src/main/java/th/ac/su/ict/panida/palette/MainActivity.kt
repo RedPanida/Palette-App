09b16e76e6865d8172263223d4af70276c2f7cc5
PanidaC <71167854+PanidaC@users.noreply.github.com>
2021-01-09 22:51:45 +0700

    Palette

---
+++ Palette/app/src/main/java/th/ac/su/ict/panida/palette/MainActivity.kt
@@ -0,0 +1,28 @@
+package th.ac.su.ict.panida.palette
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val btnO = findViewById<Button>(R.id.btnO)
+//        val btnT = findViewById<Button>(R.id.btnT)
+//        val btnTh = findViewById<Button>(R.id.btnTh)
+//        val btnF = findViewById<Button>(R.id.btnF)
+//        val btnFi = findViewById<Button>(R.id.btnFi)
+//        val btnS = findViewById<Button>(R.id.btnS)
+//
+//        btnO.setBackgroundColor(Color.rgb(235,225,222));
+//        btnT.setBackgroundColor(Color.rgb(236,203,202));
+//        btnTh.setBackgroundColor(Color.rgb(117,131,156));
+//        btnF.setBackgroundColor(Color.rgb(153,169,191));
+//        btnFi.setBackgroundColor(Color.rgb(194,204,213));
+//        btnS.setBackgroundColor(Color.rgb(221,226,229));
+
+    }
+}
